6125add6c592a8b0fa332575afb46cf32c501f44
pawanacharya73 <pawanacharya832@gmail.com>
2025-12-14 22:13:17 +0545

    Formatting, Auto Complete, Linting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,41 +1,45 @@
 return {
     {
+        -- installs and manages language servers
         "mason-org/mason.nvim",
         config = function()
             require("mason").setup()
         end
     },
     {
+        -- gives ensure_install property to mason
         "mason-org/mason-lspconfig.nvim",
         config = function()
         require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls"}
+            ensure_installed = {"lua_ls", "pylsp"}
         })
         end
     },
     {
+        -- hooks up our nvim to the LSs and establishes the connection back and forth
         "neovim/nvim-lspconfig",
         config = function()
             vim.lsp.config("lua_ls", {
                 settings = {
                     Lua = {
                         diagnostics = {
-                            globals = { "vim", "it", "describe", "before_each", "after_each" },
+                            globals = { "vim"}
                         }
                     }
                 }
             })
+
+
             vim.lsp.enable("lua_ls")
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
             vim.diagnostic.config({
-                virtual_text = {
+               virtual_text = {
                     prefix = "●",
                 }
             })
-
         end
     }
-
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,35 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require("telescope.builtin")
-        require("telescope").setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<esc>"] = builtin.close,
+    {
+        'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<esc>"] = builtin.close,
+                        },
                     },
                 },
-            },
-        })
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    end
+            })
+            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        end
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+        require("telescope").setup {
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown {
+                    }
+
+                }
+            }
+        }
+        require("telescope").load_extension("ui-select")
+        end
+    }
 }
