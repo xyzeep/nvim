854c248aea8c19868d90ecaba1a846e834ac3c5c
pawanacharya73 <pawanacharya832@gmail.com>
2025-12-22 21:55:23 +0545

    lsp-config tweak

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,44 @@
 return {
-    {
-        -- installs and manages language servers
-        "mason-org/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        -- gives ensure_install property to mason
-        "mason-org/mason-lspconfig.nvim",
-        config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls", "pylsp", "ts_ls", "eslint", "emmet_ls", "clangd"}
-        })
-        end
-    },
-    {
-        -- hooks up our nvim to the LSs and establishes the connection back and forth
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.lsp.config("lua_ls", {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim"}
-                        }
-                    }
-                }
-            })
+	{
+		-- installs and manages language servers
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- gives ensure_install property to mason
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pylsp", "ts_ls", "eslint", "emmet_ls"},
+			})
+		end,
+	},
+	{
+		-- hooks up our nvim to the LSs and establishes the connection back and forth
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
+			vim.lsp.enable("lua_ls")
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-            vim.lsp.enable("lua_ls")
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-
-            vim.diagnostic.config({
-               virtual_text = {
-                    prefix = "●",
-                }
-            })
-        end
-    }
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+				},
+			})
+		end,
+	},
 }
